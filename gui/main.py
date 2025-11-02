b57fd640ec956c085b348f9ced6d51f35b32652c
aronyx0 <aronyx0@gmail.com>
2025-11-02 21:56:39 +0100

    Optimization of GUI, not done yet

--- gui/main.py
+++ gui/main.py
@@ -2,12 +2,15 @@ import serial
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
+import matplotlib.style as style
 import time
 import tester
 
-testing = False
-monitor = serial.Serial("COM5")
+testing = True
+if not testing:
+    monitor = serial.Serial("COM5")
 buffer = ""
+yawdist = {90: 100}
 
 def last_serial_line():
     global buffer, monitor
@@ -30,7 +33,6 @@ class ArduinoData:
         if testing:
             datastr = tester.inputdata()
         else:
-            time.sleep(1)
             datastr = str(eval(last_serial_line()))
         datastr = eval(datastr)
         self.yaw = datastr[0]
@@ -41,35 +43,39 @@ def graph_init(data: "ArduinoData") -> None:
     fig = plt.figure(figsize=(6, 6))
     ax = plt.subplot(111, polar=True)
     ax.set_xlim(0, np.pi)
-    ax.set_ylim(0, 200)
-    
-    theta = list(range(0, 181))
-    r = list(range(0, 181))
+    ax.set_ylim(0, 300)
+
     theta_now = data.yaw
     r_now = data.distance
-    vline = plt.axvline(theta_now, color="black")
-    graph = plt.bar(np.deg2rad(theta), r, np.deg2rad(1), color="green")
+    vline = ax.axvline(theta_now, color="black")
+    graph = ax.bar([], [], np.deg2rad(1), color="green")
 
 def graph_animate():
     global fig, animation
     def update(frame) -> tuple:
-        global theta_now, r, vline, graph, data
+        global theta_now, vline, graph, data, yawdist
         data.update()
         vline.remove()
-        graph.remove()
 
         theta_now = np.deg2rad(data.yaw)
         r_now = data.distance
-        r[data.yaw] = r_now
+        yawdist[theta_now] = r_now
+        theta = list(yawdist.keys())
+        r = list(yawdist.values())
 
         vline = plt.axvline(theta_now, color="black")
-        graph = plt.bar(np.deg2rad(theta), r, np.deg2rad(1), color="green")
+        print(graph)
         return vline, graph
 
+    style.use("fast")
     animation = anim.FuncAnimation(fig, update, interval=20, cache_frame_data=False)
 
 if __name__ == "__main__":
-    data = ArduinoData()
-    graph_init(data)
-    graph_animate()
-    plt.show()
+    try:
+        data = ArduinoData()
+        graph_init(data)
+        graph_animate()
+        plt.show()
+    except KeyboardInterrupt:
+        plt.close()
+        exit()
--- log/log (albert).md
+++ log/log (albert).md
@@ -6,3 +6,22 @@
 **2025/okt/16**:
 > Serial reading met Python getest
 > * Geeft schijnbaar bytestring, opgelost met `eval()`
+
+**2025/okt/23**:
+> Gestart met grafiek van GUI met matplotlib
+> * Documentatie gelezen, is een erg ingewikkelde library
+
+**2025/okt/24**:
+> Grafiek werkend gekregen, laat nu een statische polar plot zien
+> * Internet laat voor alles meerdere opties zien om iets te doen, daarom eerst achterkomen waarom en hoe het werkt
+
+**2025/okt/26**:
+> Aan het uitzoeken hoe je de grafiek kan laten veranderen (door `matplitlib.animation.FuncAnimation`)
+> Betere manier gemaakt voor serial-data aflezen, met een self-updating class
+
+**2025/okt/31**:
+> Begonnen aan `tkinter`-interface, nu tijdelijk nog een lege window
+
+**2025/nov/02**:
+> Logboek geüpdate vanaf locaal bestand
+> Grafiek laat nu gaten zien door lage updatetijd, dat aan het fixen
