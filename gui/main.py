38c53b3fe21a489fb3b9431bcb35c6d87ac9a7d8
aronyx0 <aronyx0@gmail.com>
2025-11-10 20:37:45 +0100

    Adding start menu to gui, unfinished

--- gui/main.py
+++ gui/main.py
@@ -1,15 +1,18 @@
 import serial
+import serial.tools.list_ports
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
 import matplotlib.style as style
 import matplotlib.colors as mplcolors
+import matplotlib.backends.backend_tkagg as mpl_backend
 import tkinter as tk
 from tkinter import ttk
 
 import tester
 
-testing = False
+testing = True
 if not testing:
     monitor = serial.Serial("COM5")
 buffer = ""
@@ -17,11 +20,13 @@ yawdist = {90: 100}
 colormap = mplcolors.LinearSegmentedColormap.from_list("", ["firebrick","orange", "green","mediumblue"])
 
 class ArduinoData:
-    def __init__(self) -> None:
+    def __init__(self, testing: bool = False, comport: str = None) -> None:
+        self.testing = testing
+        self.comport = comport
         self.update()
 
     def __str__(self) -> str:
-        return f"({self.yaw}, {self.distance})"
+        return f"({self.yaw}, {self.distance}) @ COM{self.comport}"
 
     def update(self) -> None:
         if testing:
@@ -33,16 +38,14 @@ class ArduinoData:
         self.distance = datastr[1]
 
 def graph_init(data: "ArduinoData") -> None:
+    global fig
+    fig = plt.figure(figsize=(8, 6))
+
+def graph_init_p2() -> None:
     global fig, ax, theta_now, r_now, vline, graph, yawdist
-    fig = plt.figure(figsize=(6, 6))
     ax = plt.subplot(111, polar=True)
     ax.set_xlim(0, np.pi)
     ax.set_ylim(0, 300)
-
-    theta_now = data.yaw
-    r_now = data.distance
-    vline = ax.axvline(theta_now, color="black")
-    graph = ax.scatter([], [], color="green")
 
 def animate(frame) -> tuple:
     global fig, data, yawdist, vline, graph, ax, colormap
@@ -66,11 +69,49 @@ def animate(frame) -> tuple:
                        cmap=colormap)
     return graph, vline
 
-def render_window() -> None:
+def start_graph() -> None:
+    global data, root, fig, vline, r_now, theta_now, graph, animation
+    data = ArduinoData()
+    graph_init(data)
+    canvas = mpl_backend.FigureCanvasTkAgg(fig, root)
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+    graph_init_p2()
+    theta_now = data.yaw
+    r_now = data.distance
+    vline = ax.axvline(theta_now, color="black")
+    graph = ax.scatter([], [], color="green")
+    style.use("fast")
+    animation = anim.FuncAnimation(fig, animate, interval=10, cache_frame_data=False, blit=True)
+
+def render_graph_window() -> None:
+    global root
     root = tk.Tk()
+    width = 768
+    height = 576
+    root.minsize(width, height)
+
+    comport_menu = tk.Listbox(root)
+    comports = serial.tools.list_ports.comports()
+    for port in comports:
+        comport_menu.insert("end", port)
+    comport_menu.pack()
+    print(comports)
+
+    start_button = ttk.Button(root, text="Start", command=lambda: [start_graph(), start_button.pack_forget()])
+    start_button.pack()
+
+    root.protocol("WM_DELETE_WINDOW", lambda: [root.destroy(), exit()])
+    root.mainloop()
 
 if __name__ == "__main__":
     try:
+        render_graph_window()
+    except KeyboardInterrupt:
+        root.destroy()
+        exit()
+
+    """try:
         data = ArduinoData()
         graph_init(data)
         #graph_animate()
@@ -80,4 +121,4 @@ if __name__ == "__main__":
         
     except KeyboardInterrupt:
         plt.close()
-        exit()
+        exit()"""
--- gui/tkintertest.py
+++ gui/tkintertest.py
@@ -1,19 +1,34 @@
-import tkinter as tk
-from tkinter import ttk
+# Source - https://stackoverflow.com/a
+# Posted by HYRY, modified by community. See post 'Timeline' for change history
+# Retrieved 2025-11-07, License - CC BY-SA 4.0
 
-def fn():
-    pass
+#---------Imports
+from numpy import arange, sin, pi
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+import tkinter as Tk
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+#---------End of imports
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    root.title("Lorem Ipsum")
-    root.geometry("600x600")
-    root.resizable(False, False)
-    contents = []
+fig = plt.Figure()
 
-    contents.append(ttk.Button(root, text="quit", command=lambda: root.quit()))
-    contents.append(ttk.Label(root, text="hell yeah"))
-    for item in contents:
-        item.pack()
+x = np.arange(0, 2*np.pi, 0.01)        # x-array
 
-    root.mainloop()
+def animate(i):
+    line.set_ydata(np.sin(x+i/10.0))  # update the data
+    return line,
+
+root = Tk.Tk()
+
+label = Tk.Label(root,text="SHM Simulation").grid(column=0, row=0)
+
+canvas = FigureCanvasTkAgg(fig, master=root)
+canvas.get_tk_widget().grid(column=0,row=1)
+
+ax = fig.add_subplot(111)
+line, = ax.plot(x, np.sin(x))
+ani = animation.FuncAnimation(fig, animate, np.arange(1, 200), interval=25, blit=False)
+
+Tk.mainloop()
